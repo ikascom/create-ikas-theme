092677200f17eda0bd7b34c03fb6dea780a5b09b
Said Furkan <dizefurkan@gmail.com>
2022-09-21 10:10:35 +0300

    [REFACTOR] Package.json clear for new 4.0 version

--- templates/typescript/src/components/__generated__/editor.tsx
+++ templates/typescript/src/components/__generated__/editor.tsx
@@ -2,10 +2,11 @@ import dynamic from "next/dynamic";
 import { IkasEditorComponentLoader } from "@ikas/storefront";
 
 
+const Component0 = dynamic(() => import("../start"), { loading: () => <IkasEditorComponentLoader /> });
 
 
 const Components = {
-  
+  "28626d46-97f8-45e7-9c25-dcb16d618676": Component0
 };
 
 export default Components;
