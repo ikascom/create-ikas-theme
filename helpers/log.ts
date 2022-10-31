37b3520e6cddae1804c9c5e42c448c08bc83f329
Umut Ozan Yıldırım <uoy1991@gmail.com>
2022-10-31 10:31:16 +0300

    npm commands changed to yarn.

--- helpers/log.ts
+++ helpers/log.ts
@@ -18,16 +18,16 @@ export function greeting() {
 
 export function success({ folderName }: { folderName: string }) {
   console.log(`\nInside that directory, you can run several commands:\n
-\x1b[36mnpm run dev\x1b[0m
+\x1b[36myarn dev\x1b[0m
   Starts the development server.
 
-\x1b[36mnpm run build\x1b[0m
+\x1b[36myarn build\x1b[0m
   Bundles the app into static files for production.
 
 We suggest that you begin by typing:
 
 \x1b[36mcd ${folderName}
-npm install\x1b[0m
+yarn install\x1b[0m
 
 Happy coding ⚡️`);
 }
